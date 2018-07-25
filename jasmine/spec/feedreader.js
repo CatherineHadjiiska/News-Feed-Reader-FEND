98ef2cfd06668a89ff905b73ca831d212024bc8e
Catherine Hadjiiska <catherine.hadjiiska@gmail.com>
2018-07-25 11:10:59 +0300

    allFeeds objects have URL and name.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -38,5 +38,18 @@ $(function() {
             });
          });
 
+         
+            /* TODO: Write a test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+         it('each feed name is defined and not empty', function() {
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+         });
+    });
+
 
 
