66b1e3b0ac3fd073820d15fae2d1f22acbb8ad4e
Catherine Hadjiiska <catherine.hadjiiska@gmail.com>
2018-07-25 11:13:09 +0300

    "The menu" should be hidden by default, visibility on click

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -38,7 +38,7 @@ $(function() {
             });
          });
 
-         
+
             /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
@@ -49,6 +49,35 @@ $(function() {
                 expect(feed.name.length).not.toBe(0);
             });
          });
+    });
+
+    /* TODO: Write a new test suite named "The menu" */
+    describe ('The menu', function() {
+         /* TODO: Write a test that ensures the menu element is
+         * hidden by default. You'll have to analyze the HTML and
+         * the CSS to determine how we're performing the
+         * hiding/showing of the menu element.
+         */
+         it('menu element should be hidden by default', function() {
+            var checkClass = $('body').hasClass('menu-hidden');
+            expect(checkClass).toBe(true);
+         });
+
+         /* TODO: Write a test that ensures the menu changes
+          * visibility when the menu icon is clicked. This test
+          * should have two expectations: does the menu display when
+          * clicked and does it hide when clicked again.
+          */
+
+          it('display/hide menu on click', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+
+            // second click
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
+         
     });
 
 
